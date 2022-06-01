cec17b6d540d8c1ec3897071e3e90f5d0969f677
Abdullah Alabbad <a.a.al3bad@gmail.com>
2022-06-02 02:21:34 +1000

    project: display overlay on the image (not completed)

--- lab_project_hdmi/project_hdmi.v
+++ lab_project_hdmi/project_hdmi.v
@@ -15,13 +15,13 @@ module project_hdmi (
   inout               I2C_SDA,           // I2C data
   inout               I2C_SCL,           // I2C clock
   // HDMI
-  output              HDMI_TX_CLK,       // Video clk
-  output              HDMI_TX_DE,        // Data enable signal
-  output              HDMI_TX_HS,        // Horizontal sync
-  output              HDMI_TX_VS,        // Vertical sync
-  output [23:0]       HDMI_TX_D,         // Video data
+  output              HDMI_CLK,       // Video clk
+  output              HDMI_DE,        // Data enable signal
+  output              HDMI_HS,        // Horizontal sync
+  output              HDMI_VS,        // Vertical sync
+  output [23:0]       HDMI_PX,         // Video data
   // Unused pins
-  input               HDMI_TX_INT,       // Interrupt signal
+  input               HDMI_INT,       // Interrupt signal
   output              HDMI_I2S0,         // I2S Channel 0 Audio Data Input
   output              HDMI_MCLK,         // Audio Reference Clock Input
   output              HDMI_SCLK,         // Audio Left/Right Channel SignalInput
@@ -48,11 +48,10 @@ wire [7:0] BLUE;
 assign HDMI_I2S0 = 1'b0;
 assign HDMI_MCLK = 1'b0;
 assign HDMI_SCLK = 1'b0;
-assign HDMI_TX_D = {RED, GREEN, BLUE};
 
-// assign GPIO_1[0] = HDMI_TX_DE? 1'b1 : 1'b0;
-// assign GPIO_1[1] = HDMI_TX_HS? 1'b1 : 1'b0;
-// assign GPIO_1[2] = HDMI_TX_VS? 1'b1 : 1'b0;
+// assign GPIO_1[0] = HDMI_DE? 1'b1 : 1'b0;
+// assign GPIO_1[1] = HDMI_HS? 1'b1 : 1'b0;
+// assign GPIO_1[2] = HDMI_VS? 1'b1 : 1'b0;
 
 assign GPIO_1[0] = CLK_I2C;
 assign GPIO_1[1] = I2C_SCL? 1'b1 : 1'b0;
@@ -72,7 +71,7 @@ clk_src #(
   .CTR_WIDTH_PX(2'd2),
   .CTR_PRELOAD_PX(2'd2),
   // I2C counter config
-  // For 500 kHz clock:
+  // For 250 kHz clock:
   //    num of counts  = (1/50MHz) * x = (1/250kHz) / 2 ==> x = 100 counts
   //    counter width  = 2^x = 100 ==> x = cail(6.64) = 7-bits
   //    preload value  = 2^7 - 100 = 28
@@ -94,6 +93,7 @@ parameter ADDR_WIDTH      = 4;       // 2^x = 14 = ceil(3.8) = 4-bits
 parameter I2C_SLAVE_ADDR  = 8'h72;   // I2C slave address + write command
 wire [15:0] HDMI_CONFIG_MEM [NUM_OF_CONFIG-1:0];
 wire [ADDR_WIDTH-1:0] config_addr;
+wire ready;
 
 // ROM containing ADV7513 config
 // Refernce: ADV7513 programming guide - Quick start guide - page 14
@@ -102,7 +102,7 @@ wire [ADDR_WIDTH-1:0] config_addr;
 //    - RGB colour space
 //    - 4:4:4 video format
 
-//                             reg   ACK  payload  ACK
+//                          mem_addr payload
 assign HDMI_CONFIG_MEM[0]  = {8'h15, 8'h20};
 assign HDMI_CONFIG_MEM[1]  = {8'h16, 8'h30};
 assign HDMI_CONFIG_MEM[2]  = {8'h17, 8'h00};
@@ -129,7 +129,8 @@ I2C_controller #(
   .CONFIG(HDMI_CONFIG_MEM[config_addr]),
   .config_addr(config_addr),
   .I2C_SCL(I2C_SCL),    // CLK
-  .I2C_SDA(I2C_SDA)     // DATA
+  .I2C_SDA(I2C_SDA),    // DATA
+  .ready(ready)
 );
 
 //=============================================
@@ -148,6 +149,21 @@ IMG_MEM_BW rom(
 );
 
 //=============================================
+// ==> TXT ROM
+//=============================================
+
+// Notes:
+//    - Memory size should be limited to (2^17) × 3 = 393216 becouse of hardware limitation
+wire [13:0] TXT_PX_ADDR;
+wire [23:0] TXT_PX;
+
+TXT_MEM rom_txt(
+  .clock(CLK_50MHz),
+  .address(TXT_PX_ADDR),
+  .q(TXT_PX[7:0])
+);
+
+//=============================================
 // ==> Display the img
 //=============================================
 
@@ -156,13 +172,13 @@ HDMI_controller ig (
   .RST_n(RST_n || ready),
   .PX(PX),
   .PX_ADDR(PX_ADDR),
-  .HDMI_CLK(HDMI_TX_CLK),
-  .DE(HDMI_TX_DE),
-  .HSYNC(HDMI_TX_HS),
-  .VSYNC(HDMI_TX_VS),
-  .RED(RED),
-  .GREEN(GREEN),
-  .BLUE(BLUE),
+  .TXT_PX(TXT_PX),
+  .TXT_PX_ADDR(TXT_PX_ADDR),
+  .HDMI_CLK(HDMI_CLK),
+  .DE(HDMI_DE),
+  .HSYNC(HDMI_HS),
+  .VSYNC(HDMI_VS),
+  .HDMI_PX(HDMI_PX),
   .INV(px_invert)
 );
 
--- lab_project_hdmi/src/HDMI_controller.v
+++ lab_project_hdmi/src/HDMI_controller.v
@@ -1,16 +1,16 @@
 module HDMI_controller (
   input             CLK_PX,
   input             RST_n,
-  input      [23:0] PX,
   input             INV,
+  input      [23:0] PX,
+  input      [23:0] TXT_PX,
   output reg [18:0] PX_ADDR,
+  output reg [13:0] TXT_PX_ADDR,
   output            HDMI_CLK,
   output            DE,
   output            HSYNC,
   output            VSYNC,
-  output reg [7:0]  RED,
-  output reg [7:0]  GREEN,
-  output reg [7:0]  BLUE
+  output     [23:0] HDMI_PX
 );
 
 assign HDMI_CLK = CLK_PX;
@@ -23,13 +23,13 @@ assign HDMI_CLK = CLK_PX;
 // Pixel Clk = 25 Mhz
 
 parameter H_BACK_PARCH  = 6'd48;
-parameter H_ACTIVE_AREA   = 10'd640;
+parameter H_ACTIVE_AREA = 10'd640;
 parameter H_FRONT_PARCH = 5'd16;
 parameter H_SYNC_WIDTH  = 7'd96;
 parameter H_TOTAL_PX    = H_BACK_PARCH + H_ACTIVE_AREA + H_FRONT_PARCH + H_SYNC_WIDTH;
 
 parameter V_BACK_PARCH  = 6'd33;
-parameter V_ACTIVE_AREA   = 10'd480;
+parameter V_ACTIVE_AREA = 10'd480;
 parameter V_FRONT_PARCH = 5'd10;
 parameter V_SYNC_WIDTH  = 7'd2;
 parameter V_TOTAL_PX    = V_BACK_PARCH + V_ACTIVE_AREA + V_FRONT_PARCH + V_SYNC_WIDTH;
@@ -37,64 +37,75 @@ parameter V_TOTAL_PX    = V_BACK_PARCH + V_ACTIVE_AREA + V_FRONT_PARCH + V_SYNC_
 parameter IMG_X = 640;
 parameter IMG_Y = 480;
 
+parameter MARGIN = 2;
+parameter OVERLAY_START_X = MARGIN;
+parameter OVERLAY_END_X   = OVERLAY_START_X + 100;
+
+// Hight of the letter = 10px
+// Margin between words and borders = 2px
+// Two lines = (2 * 10px) + (3 * 2px)
+parameter OVERLAY_START_Y = V_ACTIVE_AREA - 20 - (MARGIN * 2);
+parameter OVERLAY_END_Y   = V_ACTIVE_AREA - MARGIN;
+
 //=============================================
 // ==> Wires / registers
 //=============================================
 
+reg [7:0]  RED;
+reg [7:0]  GREEN;
+reg [7:0]  BLUE;
+
 reg [9:0] counter_x;
 reg [9:0] counter_y;
+reg [13:0] counter_overlay;
 
 wire end_reached_h;
 wire end_reached_v;
+
+wire active_h;
+wire active_v;
 wire active;
 
-assign end_reached_h = (counter_x == H_TOTAL_PX);
-assign end_reached_v = (counter_y == V_TOTAL_PX);
+wire active_overlay_h;
+wire active_overlay_v;
+wire active_overlay;
 
-assign active_h = (counter_x > H_BACK_PARCH && counter_x <= (H_BACK_PARCH + H_ACTIVE_AREA));
-assign active_v = (counter_y > V_BACK_PARCH && counter_y < (V_BACK_PARCH + V_ACTIVE_AREA));
-assign active = active_h && active_v;
+assign end_reached_h    = (counter_x == H_TOTAL_PX);
+assign end_reached_v    = (counter_y == V_TOTAL_PX);
 
-assign HSYNC = !(counter_x > H_TOTAL_PX - H_SYNC_WIDTH);
-assign VSYNC = !(counter_y >= V_TOTAL_PX - V_SYNC_WIDTH);
-assign DE = active;
+assign active_h         = (counter_x > H_BACK_PARCH && counter_x <= (H_BACK_PARCH + H_ACTIVE_AREA));
+assign active_v         = (counter_y > V_BACK_PARCH && counter_y < (V_BACK_PARCH + V_ACTIVE_AREA));
+assign active           = active_h && active_v;
+
+assign active_overlay_h = ((counter_x - H_BACK_PARCH) > OVERLAY_START_X) && ((counter_x - H_BACK_PARCH) <= OVERLAY_END_X);
+assign active_overlay_v = ((counter_y - V_BACK_PARCH) > OVERLAY_START_Y) && ((counter_y - V_BACK_PARCH) <= OVERLAY_END_Y);
+assign active_overlay   = active_overlay_h && active_overlay_v;
+
+assign HSYNC            = !(counter_x > H_TOTAL_PX - H_SYNC_WIDTH);
+assign VSYNC            = !(counter_y >= V_TOTAL_PX - V_SYNC_WIDTH);
+assign DE               = active;
+assign HDMI_PX          = {RED, GREEN, BLUE};
 
 
 //=============================================
-// ==> Horizontal pixels
+// ==> Horizontal & vertical counters
 //=============================================
 
-// Horizontal
 always @(posedge CLK_PX, negedge RST_n) begin
   if (!RST_n) begin
     // reset
     counter_x <= 0;
-  end
-  else begin
-    // logic
-    if (end_reached_h)
-      counter_x <= 0;
-    else
-      counter_x <= counter_x + 1'b1;
-  end
-end
-
-//=============================================
-// ==> Vertical pixels
-//=============================================
-always @(posedge CLK_PX, negedge RST_n) begin
-  if (!RST_n) begin
-    // reset
     counter_y <= 0;
   end
   else begin
-    // logic
+    // h counter
+    if (end_reached_h) counter_x <= 0;
+    else counter_x <= counter_x + 1'b1;
+    // v counter
     if (end_reached_h) begin
-      if (end_reached_v)
-        counter_y <= 0;
-      else
-        counter_y <= counter_y + 1'b1;
-      end
+      if (end_reached_v) counter_y <= 0;
+      else counter_y <= counter_y + 1'b1;
+    end
   end
 end
 
@@ -108,38 +119,42 @@ always @(posedge CLK_PX, negedge RST_n) begin
     GREEN <= 0;
     BLUE <= 0;
     PX_ADDR <= 0;
+    TXT_PX_ADDR <= 0;
+    counter_overlay <= 0;
   end
   else begin
-    // logic
+
     if (active) begin
       // Draw gradient (black-white)
       // {RED, GREEN, BLUE} <= {RED + 1'b1, GREEN + 1'b1, BLUE + 1'b1};
+      if (active_overlay) begin
+        counter_overlay <= 1'b1;
+        // Display pixels
+        {RED, GREEN, BLUE} <= {TXT_PX[7:0], TXT_PX[7:0], TXT_PX[7:0]};
 
-      // if (counter_y > IMG_Y + V_BACK_PARCH) begin
-        // reset addr when the last row of the image is reached
-      //   PX_ADDR <= 0;
-      // end
-      // else if (counter_x > IMG_X + H_BACK_PARCH)
-        // black px when drawing outside the img
-        // {RED, GREEN, BLUE} <= {8'h0, 8'h0, 8'h0};
-      // Draw img on the top-left corner
-      // else if (counter_x <= IMG_X + H_BACK_PARCH && counter_y <= IMG_Y + V_BACK_PARCH) begin
-        // {RED, GREEN, BLUE} <= {PX[23:16], PX[15:8], PX[7:0]};
-        if (INV)
-          {RED, GREEN, BLUE} <= ~{PX[7:0], PX[7:0], PX[7:0]};
-        else
-          {RED, GREEN, BLUE} <= {PX[7:0], PX[7:0], PX[7:0]};
-        PX_ADDR <= PX_ADDR + 1'b1;
-      // end
+        // Select word for the first line
+        if (counter_overlay == 0 && INV) TXT_PX_ADDR <= 14'd1100;
+        // Select word for the second line
+        else if (counter_overlay == 14'd300) TXT_PX_ADDR <= 14'd3300; // FIXME:
+        // Retrive the pixels of the current word
+        else TXT_PX_ADDR <= TXT_PX_ADDR + 1'b1;
 
+      end
+      else if (INV)
+        {RED, GREEN, BLUE} <= ~{PX[7:0], PX[7:0], PX[7:0]};
+      else
+        {RED, GREEN, BLUE} <= {PX[7:0], PX[7:0], PX[7:0]};
+      PX_ADDR <= PX_ADDR + 1'b1;
     end
     else begin
       // black px when not in active area
       {RED, GREEN, BLUE} <= {8'h00, 8'h00, 8'h00};
     end
-
-    if (end_reached_v)
+    if (end_reached_v) begin
       PX_ADDR <= 0;
+      TXT_PX_ADDR <= 0;
+      counter_overlay <= 0;
+    end
   end
 end
 
